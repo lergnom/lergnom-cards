ca08ead9713486d87df8c47c143b4cc0c6c201e3
lergnom <lergnom@mail.ru>
2021-09-16 09:23:17 +0300

    added ul for pages

--- src/01-main/ui/Header.tsx
+++ src/01-main/ui/Header.tsx
@@ -7,6 +7,36 @@ export const Header = () => {
     return (
         <>
             <ul className={s.ulWrapper}>
+                <h3>Pages:</h3>
+                <li>
+                    <NavLink to={PATH.SUPER_BUTTON} activeClassName={s.active}>
+                        Login
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink to={PATH.SUPER_INPUT} activeClassName={s.active}>
+                        Registaration
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={PATH.SUPER_CHECKBOX} activeClassName={s.active}>
+                        Profile
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={PATH.SUPER_CHECKBOX} activeClassName={s.active}>
+                        Password recovery
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={PATH.SUPER_CHECKBOX} activeClassName={s.active}>
+                        404
+                    </NavLink>
+                </li>
+            </ul>
+            <ul className={s.ulWrapper}>
+                <h3>Components:</h3>
                 <li>
                     <NavLink to={PATH.SUPER_BUTTON} activeClassName={s.active}>
                         Button example
@@ -24,6 +54,7 @@ export const Header = () => {
                     </NavLink>
                 </li>
             </ul>
+
         </>
     );
 };
