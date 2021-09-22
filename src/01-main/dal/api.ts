34eb38978fe309ebe772d096e49b4f1673b20d06
lergnom <lergnom@mail.ru>
2021-09-22 08:56:15 +0300

    add querys

--- src/01-main/dal/api.ts
+++ src/01-main/dal/api.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://neko-cafe-back.herokuapp.com/auth/',
-})
+    baseURL: 'https://neko-cafe-back.herokuapp.com/',
+});
 
 
 export const requestApi = {
-       get() {
+    get() {
         return instance.get(``);
-    }
-}
+    },
+    authLogin() {
+        return instance.post("auth/login", {email: "lergnpm@mail.ru", password: "12345678", rememberMe: true});
+    },
+    authRequest() {
+        return instance.post("auth/me");
+    },
+    deleteAuth() {
+        return instance.delete("auth/me");
+
+    },
+};
