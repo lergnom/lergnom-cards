ea4aa560c822b748151b6ff843373c936fdfeeb6
lergnom <lergnom@mail.ru>
2021-10-08 12:43:20 +0300

    change profile

--- src/02-features/f1-auth/a1-UI/a3-profile/ProfileContainer.tsx
+++ src/02-features/f1-auth/a1-UI/a3-profile/ProfileContainer.tsx
@@ -21,6 +21,30 @@ export const ProfileContainer: React.FC = () => {
                         Anton
                     </div>
                 </div>
+
+                <div className={s.navigation}>
+                    <div className={s.navigationTitle}>
+                        Колоды карт:
+                    </div>
+                    <ul>
+                        <li><a href="all" data-item="all" className="left-menu active">Мои</a></li>
+                        <li><a href="new" data-item="new" className="left-menu">Новый
+                            <div className={s.badge}>7</div>
+                        </a></li>
+                        <li><a href="work" data-item="work" className="left-menu">В работе</a></li>
+                        <li><a href="final" data-item="final" className="left-menu">Завершенный</a></li>
+                        <li><a href="arc" data-item="arc" className="left-menu">Архив</a></li>
+                    </ul>
+                </div>
+
+                <div className={s.navigation}>
+                    <ul>
+                        <li>
+                            <a href="#">Выход</a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </>
     );
