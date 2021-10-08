cbdaa2f8a757d769c8833eab93ea045a719d9439
lergnom <lergnom@mail.ru>
2021-10-08 14:51:40 +0300

    change profileComponent

--- src/02-features/f1-auth/a1-UI/a3-profile/Profile.tsx
+++ src/02-features/f1-auth/a1-UI/a3-profile/Profile.tsx
@@ -1,21 +1,48 @@
 import React from "react";
-import s from "./anypage.module.css";
+import s from "./Profile.module.css";
 
-export const Profile = () => {
+type ProfileTypeProps = {
+    title?: string,
+    subtitle?: string,
+    avatar?: string,
+    userName?: string
+}
+
+export const Profile: React.FC<ProfileTypeProps> = ({title, subtitle, avatar, userName, children}) => {
     return (
-        <>
-            <div className={s.pageWrapper}>
-                <div className={s.pageContainer}>
-                    <div className={s.pageHeader}>
-                        <p className={s.pageLogo}>
-                            <span>Profile</span> page
-                        </p>
-                    </div>
-                    <div className={s.pageHr}></div>
-                    The page is still under construction :(
+        <div className={s.leftPanel}>
 
+            <div className={s.logo}>
+                <div className={s.title}>{title}</div>
+                <div className={s.subtitle}>{subtitle}</div>
+            </div>
+
+            <div className={s.userWrap}>
+                <div className={s.userPhoto}>
+                    <img src={avatar} alt={userName}/>
+                </div>
+                <div className={s.userName}>
+                    {userName}
                 </div>
             </div>
-        </>
+
+            <div className={s.navigation}>
+                <div className={s.navigationTitle}>
+                    Колоды карт:
+                </div>
+                {children}
+                {/*<ul>*/}
+                {/*    <li><a href="all" data-item="all" className="left-menu active">Мои</a></li>*/}
+                {/*    <li><a href="new" data-item="new" className="left-menu">Новый*/}
+                {/*        <div className={s.badge}>7</div>*/}
+                {/*    </a></li>*/}
+                {/*    <li><a href="work" data-item="work" className="left-menu">В работе</a></li>*/}
+                {/*    <li><a href="final" data-item="final" className="left-menu">Завершенный</a></li>*/}
+                {/*    <li><a href="arc" data-item="arc" className="left-menu">Архив</a></li>*/}
+                {/*</ul>*/}
+            </div>
+
+
+        </div>
     );
 };
--- src/02-features/f1-auth/a1-UI/a3-profile/ProfileContainer.tsx
+++ src/02-features/f1-auth/a1-UI/a3-profile/ProfileContainer.tsx
@@ -1,51 +1,31 @@
 import React from "react";
 import s from "./ProfileContainer.module.css";
+import {useSelector} from "react-redux";
+import {AppStoreType} from "../../../../01-main/bll/store";
+import {UserType} from "../../a3-DAL/authApi";
+import {Profile} from "./Profile";
 
 export const ProfileContainer: React.FC = () => {
+
+    const user = useSelector<AppStoreType, UserType | null>(state => state.auth.user);
+
+
     return (
         <>
-            <div className={s.leftPanel}>
+            <Profile title={"CARD PACK"} subtitle={"закрепление навыков"} avatar={user?.avatar} userName={user?.name}>
+                <ul>
+                    <li><a href="">Выход</a></li>
+                </ul>
+            </Profile>
 
-                <div className={s.logo}>
-                    <div className={s.title}>CARD PACKS</div>
-                    <div className={s.subtitle}>закрепление навыков</div>
+
+            <div className={s.mainWrapper}>
+                <div>
+                    sdfsdfsdf
                 </div>
-
-                <div className={s.userWrap}>
-                    <div className={s.userPhoto}>
-                        <img src="img/avatars/avatar-128.jpg" alt="Avatar"/>
-                    </div>
-                    <div className={s.userName}>
-                        Asmolovskiy
-                        <br/>
-                        Anton
-                    </div>
-                </div>
-
-                <div className={s.navigation}>
-                    <div className={s.navigationTitle}>
-                        Колоды карт:
-                    </div>
-                    <ul>
-                        <li><a href="all" data-item="all" className="left-menu active">Мои</a></li>
-                        <li><a href="new" data-item="new" className="left-menu">Новый
-                            <div className={s.badge}>7</div>
-                        </a></li>
-                        <li><a href="work" data-item="work" className="left-menu">В работе</a></li>
-                        <li><a href="final" data-item="final" className="left-menu">Завершенный</a></li>
-                        <li><a href="arc" data-item="arc" className="left-menu">Архив</a></li>
-                    </ul>
-                </div>
-
-                <div className={s.navigation}>
-                    <ul>
-                        <li>
-                            <a href="#">Выход</a>
-                        </li>
-                    </ul>
-                </div>
-
             </div>
+
+
         </>
     );
 };
